b5f524f32d9ba5de3aa8fcfdb0417df9284c7992
Ruben8ap <ruben8ap@gmail.com>
2019-09-10 22:27:04 -0400

    Cambios mayores.
    Se agrego los condicionales, correspondientes para los tipos de datos 
    permitidos para la funcion.
    
    Sin embargo aun falta evaluar el caso para el cual la longitud de las 
    listas mas internas son distintas

--- Transpuesta.py
+++ Transpuesta.py
@@ -1,13 +1,28 @@
 def traspuesta(A):
     if type(A)==list:
-        """Funcion para Trasponer una Matriz"""
-        filas = len(A)
-        columnas = len(A[0])
-        print("Matriz Original")
-        for x in A:print(x)
-        Transposicion=[[A[j][i] for j in range(filas)] for i in range(columnas)]
-        print("Matriz Transpuesta")
-        for k in Transposicion:print(k)
+        aux=[]
+        aux_2=[]
+        for h in A:
+            if type(h)==list:
+                aux.append(h)
+            else:
+                raise TypeError("Solo puede ingresar tipo lista dentro de la funcion")
+            for j in h:
+                if type(j)==type("str"):
+                    aux_2.append(j)
+        if len(aux_2)>=1:
+            raise TypeError("No se puede ingresar tipo datos string dentro de esta funcion")
+        if len(aux_2)==0:
+            if len(A)!=len(aux):
+                print("\nLa funcion traspuesta, solo acepta elementos del tipo lista")
+            if len(A)==len(aux):
+                filas = len(A)
+                columnas = len(A[0])
+                print("\nMatriz Original\n")
+                for x in A:print(x)
+                Transposicion=[[A[j][i] for j in range(filas)] for i in range(columnas)]
+                print("\nMatriz Transpuesta\n")
+                for k in Transposicion:print(k)
     else:
-        print("El tipo de arreglo para la matrices debe ser de la forma listas de listas")
+        print("La funcion traspuesta, solo permite la entrada de tipo lista")
     return A
