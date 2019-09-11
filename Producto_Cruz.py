c3a84fe3a104090a776f9afc6ad11a345d5e2910
Ruben8ap <ruben8ap@gmail.com>
2019-09-10 23:03:39 -0400

    Correcion en el condicional principal.
    Falta agregar mas condicionales para los tipos de entrada de la funcion.

--- Producto_Cruz.py
+++ Producto_Cruz.py
@@ -1,18 +1,18 @@
 def ProductoCruz(*args):
-    if type(args)==list and type(args[0])==list:
+    Final=[]
+    Resultado=[]
+    if (type(args[0]) and type(args[0][0]) and type(args[0][1]))==list:
         if len(args[0])==2:
             if len(args[0][0])==3 and len(args[0][1])==3:
                 Multiplicacion_i=(args[0][0][1]*args[0][1][2])-(args[0][0][2]*args[0][1][1])
                 Multiplicacion_j=-( (args[0][0][0]*args[0][1][2]) - (args[0][0][2]*args[0][1][0]) )
                 Multiplicacion_k=  (args[0][0][0]*args[0][1][1]) - (args[0][0][1]*args[0][1][0])
-                Resultado=[]
                 Resultado.append(Multiplicacion_i)
                 Resultado.append(Multiplicacion_j)
                 Resultado.append(Multiplicacion_k)
-                Final=[]
                 Final.append(Resultado)
-                print(Final)
-            else:print("Usted debe ingresar vectores con 3 componentes")
-        else:print("Usted debe ingresar unicamente dos vectores.")
-    else:print("La funcion solo acepta vectores, que emulen un arreglo de listas de listas")
+                print("\nEl resultado de la multiplicacion vectorial es: {}".format(Final))
+            else:print("\nUsted debe ingresar vectores con 3 componentes")
+        else:print("\nUsted debe ingresar unicamente dos vectores.")
+    else:print("\nLa funcion solo acepta vectores, que emulen un arreglo de listas de listas")
     return args
